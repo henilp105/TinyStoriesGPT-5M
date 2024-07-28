ea6206cf5162b92556d4c5f8e9a6232eaf532ef9
Henil Panchal <henilp105@gmail.com>
2024-07-28 18:42:00 +0530

    A100.config.py

---
+++ A100.config.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+batch_size = 3072 # how many independent sequences will we process in parallel?
+block_size = 1024 # what is the maximum context length for predictions?
+max_iters = 10000
+eval_interval = 1000
+learning_rate = 1e-5
+device = 'cpu' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 256
+n_head = 8
+n_layer = 6
+dropout = 0.0
+# ------------
+
+torch.manual_seed(1337)
+
+with open('dataset.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
